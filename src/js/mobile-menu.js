44ac17f48fe1fe2361b24f5380efb3d1a83169e0
KonstantinSvizhenko <kostua.90@gmail.com>
2022-05-17 10:45:04 +0300

    full-fix-modal

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,12 +1,12 @@
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('.modal-menu__open'),
-    closeMenuBtn: document.querySelector('.modal-menu__close'),
+    closeMenuBtn: document.querySelectorAll('.modal-menu__close-js'),
     menu: document.querySelector('.modal-menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.forEach(btn => btn.addEventListener('click', toggleModal))
 
   function toggleModal() {
     refs.menu.classList.toggle('is-hidden');
